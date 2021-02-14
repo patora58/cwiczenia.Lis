7c54dfe0b52397516dc7094be24252b6a8e97c7d
patora58 <patora58@gmail.com>
2021-02-14 19:44:43 +0100

    Działania arytmetyczne

---
+++ działania artymet. lis/działania artymet. lis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace działania_artymet._lis
+{
+    public class Program
+    { 
+        public static void Main()
+    {
+            int a, b, c;
+            a = 10;
+            b = 25;
+            c = b - a;
+            Console.WriteLine("a = " +a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("b - a = " + c);
+            c = a * b;
+            Console.WriteLine("a * b = " + c);
+        }
+
