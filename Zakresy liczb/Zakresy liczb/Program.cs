ed7dbbafb93eacb0dda1b94e94f476a954fa573f
patora58 <patora58@gmail.com>
2021-02-14 19:53:26 +0100

    Zakresy liczb

---
+++ Zakresy liczb/Zakresy liczb/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Zakresy_liczb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte zmienna;
+            zmienna = 128;
+            Console.WriteLine("zmienna = " + zmienna);
+            /*Jesli jest bląd brak debugowania/* 
+        }
+    }
+}
