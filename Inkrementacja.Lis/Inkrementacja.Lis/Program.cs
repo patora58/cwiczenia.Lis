5f4ee37131295bcf8d7bd97134294661da47330a
patora58 <patora58@gmail.com>
2021-02-14 19:48:09 +0100

    Inkrementacja

---
+++ Inkrementacja.Lis/Inkrementacja.Lis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Inkrementacja.Lis
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*1*/
+            int x = 1, y;
+            /*2*/
+            Console.WriteLine(--x);
+            /*3*/
+            Console.WriteLine(x--);
+            /*4*/
+            Console.WriteLine(x);
+            /*5*/
+            y = x--;
+            /*6*/
+            Console.WriteLine(y);
+            /*7*/
+            y = --x;
+            /*8*/
+            Console.WriteLine(--y);
+
+            
+
+        }
+    }
+}
