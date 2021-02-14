1f535fad2a8bccc10d2e49986d9f96c4f074a670
patora58 <patora58@gmail.com>
2021-02-14 19:39:47 +0100

    Deklaracja zmiennych

---
+++ lis nr 1 deklaracja zmiennych/lis nr 1 deklaracja zmiennych/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lis_nr_1_deklaracja_zmiennych
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, b, c;
+            a = 10;
+            b = 25;
+            c = b % a;
+            Console.WriteLine("b % a = " + c);
+            Console.WriteLine("11 % 3 = " + (11 % 3));
+           
+            
+
+
+        }
+    }
+}
