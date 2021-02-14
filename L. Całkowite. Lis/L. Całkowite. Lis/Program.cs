58b39e937e65e75afeecd81690b15b5a0c59581d
patora58 <patora58@gmail.com>
2021-02-14 19:49:05 +0100

    Całkowite

---
+++ L. Całkowite. Lis/L. Całkowite. Lis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace L._Całkowite._Lis
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, b, c;
+            a = 8;
+            b = 3;
+            c = 2;
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("a / b = " + a / b);
+            Console.WriteLine("a / c = " + a / c);
+            Console.WriteLine("b / c = " + b / c);
+
+           /* Działania na liczbach całkowitych, 
+             jeśli jest ułamek zaokrągla program do niższej liczby*/
+
+        }
+    }
+}
