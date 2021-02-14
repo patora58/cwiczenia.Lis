daa8ff4d8b03645d01c28e344ad2d67fef6d6926
patora58 <patora58@gmail.com>
2021-02-14 19:47:19 +0100

    Modulo

---
+++ modulo/modulo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace modulo
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, b, c;
+            a = 10;
+            b = 25;
+            c = b % a;
+            Console.WriteLine("b % a = " + c);
+            Console.WriteLine("11 % 3 = " + 11 % 3);
+            c = a * b;
+            Console.WriteLine("(a * b) % 120 = " + c % 120);
+
+        }
+    }
+}
