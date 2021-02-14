3a145ac0a462958059c602dacc0710b3ec076a98
patora58 <patora58@gmail.com>
2021-02-14 19:46:27 +0100

    Działąnia

---
+++ działania lis/działania lis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace działania_lis
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, b, c;
+            a = 10;
+            b = 25;
+            c = b - a;
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("b - a = " + c);
+            c = a * b;
+            Console.WriteLine("a * b = " + c);
+            c = b % a;
+            Console.WriteLine("a % b = " + c);
+
+
+
+        }
+    }
+}
