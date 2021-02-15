df66dc134d986ff82e148c84422548e3d361ba94
patora58 <patora58@gmail.com>
2021-02-15 20:53:53 +0100

    operator warunkowy.LIs

---
+++ operator waunkowy.Lis/operator waunkowy.Lis/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace operator_waunkowy.Lis
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            int x = 1, y;
+            y = (x == 2) ? 10 : 20;
+            Console.WriteLine("y = " + y);
+        }
+    }
+}
